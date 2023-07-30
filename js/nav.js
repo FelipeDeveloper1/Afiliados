de33ceb8a377b90250de99f2704bbd6727d9fa49
Felipe <felipe_oliveira2019@hotmail.com>
2023-07-30 06:22:27 -0300

    adicionando o js

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+class sidebar {
+    constructor(lista, menu) {
+        this.document.querySelectorAll(li)
+        this.document.querySelector(ul)
+    }
+    init = () => {
+
+    }
+}
+
+const menu = new sidebar(li, ul)
